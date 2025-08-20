b152902c08a514ef32262cdc1bcfe623747b4118
Shams-MHD <mhmd123@gmail.com>
2025-08-20 12:49:16 +0300

    clear some comments

--- src/common/upload/upload.service.ts
+++ src/common/upload/upload.service.ts
@@ -8,7 +8,6 @@ import {
 } from '@azure/storage-blob';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-// import { uuid } from 'uuidv4';
 import { v4 as uuid } from 'uuid';
 import * as fs from 'fs';
 import * as fss from 'fs/promises';
@@ -50,31 +49,15 @@ export class UploadService {
     const file_name = uuid() + '.' + extension;
     const blockBlobClient = await this.getBlobClient(file_name);
 
-    // new new
     const stream = fs.createReadStream(file.path);
     const bufferSize = 4 * 1024 * 1024; // 4MB
     const maxConcurrency = 20;
-
-    // await fss.unlink(file.path);
 
     try {
       await blockBlobClient.uploadStream(stream, bufferSize, maxConcurrency, {
         blobHTTPHeaders: { blobContentType: file.mimetype },
       });
 
-      // // new
-      // const stream = file.stream;
-      // const bufferSize = 4 * 1024 * 1024; // 4MB chunks
-      // const maxConcurrency = 20;
-      //
-      // await blockBlobClient.uploadStream(stream, bufferSize, maxConcurrency, {
-      //   blobHTTPHeaders: { blobContentType: file.mimetype },
-      // });
-
-      // // Upload the file buffer to Azure Blob Storage
-      // await blockBlobClient.uploadData(file.buffer);
-
-      // After a successful upload, generate a signed URL for secure access
       const signedUrl = await this.getSignedUrl(file_name);
 
       return { file_name, signedUrl };
@@ -104,50 +87,6 @@ export class UploadService {
 
     return `${blobClient.url}?${sas}`;
   }
-
-  // public async uploadFile(file: Express.Multer.File) {
-  //   this.containerName = this.configService.get('AZURE_STORAGE_CONTAINER');
-  //   const extension = file.originalname.split('.').pop();
-  //   const file_name = uuid() + '.' + extension;
-  //   const blockBlobClient = await this.getBlobClient(file_name);
-  //   const fileUrl = blockBlobClient.url;
-  //   await blockBlobClient.uploadData(file.buffer);
-  //
-  //   return fileUrl;
-  // }
-
-  // async deleteFile(file_name: string, containerName: string) {
-  //   try {
-  //     this.containerName = containerName;
-  //     const blockBlobClient = await this.getBlobClient(file_name);
-  //     await blockBlobClient.deleteIfExists();
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-  //
-  // public async downloadFile(file_name: string, containerName: string) {
-  //   this.containerName = containerName;
-  //   const blockBlobClient = await this.getBlobClient(file_name);
-  //   const downloadBlockBlobResponse = await blockBlobClient.download();
-  //   const downloaded = (
-  //     await this.streamToBuffer(downloadBlockBlobResponse.readableStreamBody)
-  //   ).toString();
-  //   return downloaded;
-  // }
-  //
-  // private streamToBuffer(readableStream) {
-  //   return new Promise((resolve, reject) => {
-  //     const chunks = [];
-  //     readableStream.on('data', (data) => {
-  //       chunks.push(data instanceof Buffer ? data : Buffer.from(data));
-  //     });
-  //     readableStream.on('end', () => {
-  //       resolve(Buffer.concat(chunks));
-  //     });
-  //     readableStream.on('error', reject);
-  //   });
-  // }
 
   public async downloadFile(fileName: string, res: Response) {
     const blockBlobClient = await this.getBlobClient(fileName);
